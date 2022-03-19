4b10223ed1a3b338b03f46635249a960d3f6bc0d
lawrecks <ugobugo2011@gmail.com>
2022-03-19 15:10:02 +0100

    chore:setup codebase

---
+++ app/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+import express from 'express';
+import config, { initConfig } from './config';
+import Logger from './config/logger';
+
+const app = express();
+const host = config.HOST;
+const port = config.PORT || 3033;
+const apiVersion = config.API_VERSION || 'v1';
+
+const logger = Logger.createLogger({ label: 'LANNISTER-API' });
+global.logger = logger;
+
+initConfig(app);
+
+logger.info(`Server started at ${host}:${port}/api/${apiVersion}/`);
+
+export default app;
